10331b41e4a6244e9066946720523b2e51c4edfc
ZZDome <zdome7z@yahoo.de>
2022-04-10 16:03:40 +0200

    styling main menu

--- script.js
+++ script.js
@@ -8,10 +8,50 @@ function showMainMenu(){
     content.innerHTML += templateMainMenu();
 }
 
+function loadGame(){
+    drawField();
+}
+
+function drawField(){
+    let content = document.getElementById('content');
+    content.innerHTML = ``;
+    content.innerHTML += templatePlayfield();
+}
+
 //Templates-----------------------------------------------------------------------------------
 
 function templateMainMenu(){
     return /* html */ `
-        
+    <div class="mainMenuContent">
+        <h1>Tic Tac Toe</h1>
+        <button onclick="loadGame()" type="button" class="btn btn-secondary mainMenuBtn">Play</button>
+        <button type="button" class="btn btn-secondary mainMenuBtn">Placeholder</button>
+        <button type="button" class="btn btn-secondary mainMenuBtn">Options</button>
+        <button type="button" class="btn btn-secondary mainMenuBtn">Credits</button>
+    </div>
+    `;
+}
+
+function templatePlayfield(){
+    return /* html */ `
+    <div class="playfieldContent">
+        <table>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
     `;
 }
