5431ece52040adfff6e4d198e58fbcd70113869a
ZZDome <zdome7z@yahoo.de>
2022-04-10 13:35:00 +0200

    adding bootstrap

---
+++ script.js
@@ -0,0 +1,17 @@
+function init(){
+    showMainMenu();
+}
+
+function showMainMenu(){
+    let content = document.getElementById('content');
+    content.innerHTML = ``;
+    content.innerHTML += templateMainMenu();
+}
+
+//Templates-----------------------------------------------------------------------------------
+
+function templateMainMenu(){
+    return /* html */ `
+        
+    `;
+}
