8f9b22b2517cbca8ad6a6baa2fa266c9eb4f728f
ZZDome <zdome7z@yahoo.de>
2022-04-13 12:35:59 +0200

    added function

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function fillShape(id) {
     }
     fields[id] = currentShape;
     drawShape();
+    checkWin();
 }
 
 function drawShape() {
@@ -41,6 +42,46 @@ function drawShape() {
         if (fields[i] == 'cross') {
             document.getElementById('cross' + i).classList.remove('d-none');
         }
+    }
+}
+
+function checkWin(){
+    let winner;
+
+    if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]){
+        winner = fields[0];
+    }
+
+    if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]){
+        winner = fields[3];
+    }
+
+    if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]){
+        winner = fields[6];
+    }
+
+    if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]){
+        winner = fields[0];
+    }
+
+    if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]){
+        winner = fields[1];
+    }
+
+    if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]){
+        winner = fields[2];
+    }
+
+    if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]){
+        winner = fields[0];
+    }
+
+    if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]){
+        winner = fields[2];
+    }
+
+    if(!!winner){
+        console.log('GEWONNEN', winner);
     }
 }
 
